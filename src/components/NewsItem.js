e65dcfb99d81be0502bd1acc1627b6c95951f2db
Amol Dogra <amol88658@gmail.com>
2021-12-30 23:01:13 +0530

    API fetching

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
         return (
             <div>
                 <div className="card" style={{width: "18rem"}}>
-  <img src={imageUrl} className="card-img-top" alt="..."/>
+  <img src={!imageUrl?"https://img.republicworld.com/republic-prod/storie…promolarge/xhdpi/dy7fjcf9ca1c1ujr_1640859794.jpeg":imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/newsdetail/" className="btn btn-sm btn-primary">Read More</a>
+    <p className="card-text">{description}...</p>
+    <a href={newsUrl} target= "_blank" className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
             </div>
