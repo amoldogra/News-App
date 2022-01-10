e24f14ead6c7f151455fec673f8e471eb417698e
Amol Dogra <amol88658@gmail.com>
2022-01-10 23:51:38 +0530

    Categories added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+BrowserRouter as Router,
+Switch,
+Route,
+} from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <Navbar/>
-        <News pageSize={8} country="in" category="science"/>
-       
+        
+        <Switch>
+          <Route path="/"><News pageSize={8} country="in" category="general"/></Route>
+          <Route path="/buisness"><News pageSize={8} country="in" category="buisness"/></Route>
+          <Route path="/entertainment"><News pageSize={8} country="in" category="entertainment"/></Route>
+          <Route path="/sports"><News pageSize={8} country="in" category="sports"/></Route>
+          <Route path="/technology"><News pageSize={8} country="in" category="technology"/></Route>
+          <Route path="/health"><News pageSize={8} country="in" category="health"/></Route>
+          <Route path="/science"><News pageSize={8} country="in" category="science"/></Route>
+          <Route path="/general"><News pageSize={8} country="in" category="general"/></Route>
+        </Switch>
+        </Router>
       </div>
     )
   }
